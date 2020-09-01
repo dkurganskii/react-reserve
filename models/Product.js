00db6f54d5537991568642c07cae720bc89c5ce8
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-09-01 11:49:04 -0500

    create products collection, medel product data

---
+++ models/Product.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const { String, Number } = mongoose.Schema.Types;
+
+const ProductSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	price: {
+		type: Number,
+		required: true
+	},
+	sku: {
+		type: String,
+		unique: true
+	},
+	description: {
+		type: String,
+		required: true
+	},
+	mediaUrl: {
+		type: String,
+		required: true
+	}
+});
+export default mongoose.models.Product || mongoose.model('Product', ProductSchema);
+
+// const Product = mongoose.model('Product', ProductSchema);
+// module.exports = Product;
