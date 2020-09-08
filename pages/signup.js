f9d7b39688708f0b2cf5b58040c35d581b685999
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-09-07 21:26:11 -0500

    build signup form

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,86 @@
+import React from 'react';
+import { Form, Input, TextArea, Button, Image, Message, Header, Icon, Segment } from 'semantic-ui-react';
+import axios from 'axios';
+import baseUrl from '../utils/baseUrl';
+import catchErrors from '../utils/catchErrors';
+import Link from 'next/link';
+
+const INITIAL_USER = {
+	name: '',
+	email: '',
+	password: ''
+};
+
 function Signup() {
-  return <>signup</>;
+	const [ user, setUser ] = React.useState(INITIAL_USER);
+	const [ disabled, setDisabled ] = React.useState(true);
+
+	React.useEffect(
+		() => {
+			const isUser = Object.values(user).every((el) => Boolean(el));
+			isUser ? setDisabled(false) : setDisabled(true);
+		},
+		[ user ]
+	);
+
+	function handleChange(event) {
+		const { name, value } = event.target;
+		setUser((prevState) => ({ ...prevState, [name]: value }));
+	}
+
+	return (
+		<React.Fragment>
+			<Message attached icon="settings" header="Get Started!" content="Create a new account" color="teal" />
+			<Form>
+				<Segment>
+					<Form.Input
+						fluid
+						icon="user"
+						iconPosition="left"
+						label="Name"
+						placeholder="Name"
+						name="name"
+						value={user.name}
+						onChange={handleChange}
+					/>
+
+					<Form.Input
+						fluid
+						icon="envelope"
+						iconPosition="left"
+						label="Email"
+						placeholder="Email"
+						name="email"
+						type="email"
+						value={user.email}
+						onChange={handleChange}
+					/>
+
+					<Form.Input
+						fluid
+						icon="lock"
+						iconPosition="left"
+						label="Password"
+						placeholder="Password"
+						name="password"
+						type="password"
+						value={user.password}
+						onChange={handleChange}
+					/>
+
+					<Button disabled={disabled} icon="signup" type="submit" color="orange" content="Signup" />
+				</Segment>
+			</Form>
+			<Message attached="bottom" warning>
+				<Icon name="help" />
+				Existing user?{' '}
+				<Link href="/login">
+					<a>Log in here</a>
+				</Link>{' '}
+				instead.
+			</Message>
+		</React.Fragment>
+	);
 }
 
 export default Signup;
