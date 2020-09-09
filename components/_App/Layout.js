9359aac622b8e5fb7a5bacff07ec10c0bf70876a
Dmitrii Kurganskii <44755635+dkurganskii@users.noreply.github.com>
2020-09-09 17:05:03 -0500

    get current user from token, protect auth routes

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -1,29 +1,25 @@
-import Head from "next/head";
-import { Container } from "semantic-ui-react";
+import Head from 'next/head';
+import { Container } from 'semantic-ui-react';
+import Header from './Header';
+import HeadContent from './HeadContent';
 
-import Header from "./Header";
-import HeadContent from "./HeadContent";
-
-function Layout({ children }) {
-  return (
-    <>
-      <Head>
-        <HeadContent />
-        {/* Stylesheets */}
-        <link rel="stylesheet" type="text/css" href="/static/styles.css" />
-        <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-        <link
-          rel="stylesheet"
-          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
-        />
-        <title>ReactReserve</title>
-      </Head>
-      <Header />
-      <Container text style={{ paddingTop: "1em" }}>
-        {children}
-      </Container>
-    </>
-  );
+function Layout({ children, user }) {
+	return (
+		<React.Fragment>
+			<Head>
+				<HeadContent />
+				{/* Stylesheets */}
+				<link rel="stylesheet" type="text/css" href="/static/styles.css" />
+				<link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+				<link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css" />
+				<title>ReactReserve</title>
+			</Head>
+			<Header user={user} />
+			<Container text style={{ paddingTop: '1em' }}>
+				{children}
+			</Container>
+		</React.Fragment>
+	);
 }
 
 export default Layout;
